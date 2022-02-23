95da7fb91d7b62982a99461ef2aa6dd677cdb3bd
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-02-22 20:47:55 -0500

    feat(es6revision): Get posts from jsonplaceholder api using axios

--- es6-revision/promises.js
+++ es6-revision/promises.js
@@ -1,1 +1,10 @@
 import axios from 'axios';
+const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+function getPosts(){
+  axios.get(apiUrl)
+        .then(response => console.log('response using axios: ', response.data))
+        .catch(error => console.log('Error: ', error));
+}
+
+getPosts();
