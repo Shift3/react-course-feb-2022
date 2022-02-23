d2ec5707d09f262812e26ba1876080d4e87ff9be
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-02-22 20:30:50 -0500

    feat(es6revision): Examples of using classes

--- class.js
+++ class.js
@@ -7,4 +7,23 @@ export class Animal{
   makeNoise(sound = 'Loud noise'){
     console.log(sound);
   }
+
+  static returnTen(){
+    return 10;
+  }
+
+  get metaData(){
+    return `This animal of type: ${this.type}, and it has ${this.legs} legs.`;
+  }
+}
+
+export class Dog extends Animal{
+  constructor(type, legs, tail){
+    super(type, legs);
+    this.tail = tail;
+  }
+
+  makeNoise(sound='Bark'){
+    console.log(sound);
+  }
 }
--- class1.js
+++ class1.js
@@ -1,4 +1,4 @@
-import { Animal } from './class.js';
+import { Animal, Dog } from './class.js';
 
 const cat = new Animal('Cat', 4);
 
@@ -6,3 +6,10 @@ console.log(cat.type);
 console.log(cat.legs);
 
 cat.makeNoise('Mew');
+
+console.log(Animal.returnTen());
+console.log(cat.metaData);
+
+const doggy = new Dog('Dog', 4);
+console.log(doggy.type, doggy.legs);
+doggy.makeNoise();
