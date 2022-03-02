f488bef4e4004f72690b29fb927b9bdbf454dab2
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-03-01 20:13:20 -0500

    feat(core-concept): Optimise calls to the api by introdung local cache for breed list

--- 04-core-concept/src/useBreedList.js
+++ 04-core-concept/src/useBreedList.js
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react";
 
+const localCache = {};
+
 export default function useBreedList(animal) {
   const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
 
   useEffect(() => {
     if (!animal) {
       setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
     } else {
       requestBreedList();
     }
@@ -13,12 +18,15 @@ export default function useBreedList(animal) {
 
   async function requestBreedList() {
     setBreedList([]);
+    setStatus("loading");
     const results = await fetch(
       `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
     );
     const json = await results.json();
-    setBreedList(json.breeds);
+    localCache[animal] = json.breeds || [];
+    setBreedList(localCache[animal]);
+    setStatus("loaded");
   }
 
-  return [breedList];
+  return [breedList, status];
 }
