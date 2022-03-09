4f1ab9c6ae064b1dc793ac54f18bebd2aec5f380
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-03-08 20:30:26 -0500

    feat(add-student): Implment students list component

--- 06-add-students/src/App.js
+++ 06-add-students/src/App.js
@@ -1,8 +1,25 @@
-import './App.css';
+import { useState } from 'react';
 import AddStudent from './components/Students/AddStudent';
+import StudentsList from './components/Students/StudentsList';
+
 function App() {
+  const [studentsList, setStudentsList] = useState([]);
+
+  const addStudentHandler = (fullname, age) => {
+    setStudentsList(prevList => {
+      return [{
+        fullname,
+        age,
+        id: Math.random().toString()
+      },...prevList]
+    });
+  }
+
   return (
-    <AddStudent />
+    <>
+      <AddStudent onAddStudent = {addStudentHandler}/>
+      <StudentsList students={studentsList}/>
+    </>
     );
 }
 
