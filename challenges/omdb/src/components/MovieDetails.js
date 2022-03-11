5c953ed03f43d0697b37565d9c0af8bb40c35aba
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-03-10 21:07:49 -0500

    Provide a solution for challenge day 4

--- challenges/omdb/src/components/MovieDetails.js
+++ challenges/omdb/src/components/MovieDetails.js
@@ -1,34 +1,48 @@
-import "./MovieDetails.css";
+import { useEffect, useState } from "react";
+import "./movieDetails.css";
+import {getMovieDetailsById} from "../utils"
 
-const MovieDetails = ({ posterUrl, title, rated, runtime, genre, plot, actors, rating}) => {
-  return (
-    <div className="container-details">
-      <img src={posterUrl} alt={title} />
-      <div className="movie-details">
-        <h2>{title}</h2>
-        <h1 style={{color: 'blue'}}>{rating}</h1>
-        
+const MovieDetails = ({movieId})=>{
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    getMovieDetailsById(movieId).then( data =>{ 
+      setMovie({...data})
+    }).catch( err => {
+      console.error("An error occured: ", err)
+      setMovie({});
+    });
+  }, [movieId])
+
+  return(
+    <div className="movie">
+      <img src={movie.Poster} alt={movie.Title} style={{paddingRight:'2rem'}} />
+      
+      <div className="movieDetail">
+        <h2>{movie.Title}</h2>
+        <h1 style={{color: 'blue'}}>{movie.imdbRating}</h1>
         <div>
-          <span>{rated}</span>
-          <span>{runtime}</span>
+          <span>{movie.Rated}</span>
+          <span>{movie.Runtime}</span>
         </div>
 
         <div>
-          <span>{genre}</span>
+          <span>{movie.Genre}</span>
         </div>
 
         <div>
           <h4>Plot</h4>
-          <p>{plot}</p>
+          <p>{movie.Plot}</p>
         </div>
 
         <div>
           <h4>Actors</h4>
-          <p>{actors}</p>  
-        </div>        
+          <p>{movie.Actors}</p>
+        </div>
       </div>
+  
     </div>
-  );
+  )
 }
 
 export default MovieDetails;
