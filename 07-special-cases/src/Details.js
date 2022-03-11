f9f0a8978608a76d9ad9cde8f4865ceb7888866b
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-03-10 20:52:12 -0500

    feat(special-cases): Implement modal using Portal

--- 07-special-cases/src/Details.js
+++ 07-special-cases/src/Details.js
@@ -3,9 +3,11 @@ import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBounday from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
+import Modal from "./Modal";
+import classes from "./Details.module.css";
 
 class Details extends Component {
-  state = { loading: true };
+  state = { loading: true, showModal: false };
 
   async componentDidMount() {
     const results = await fetch(
@@ -15,12 +17,17 @@ class Details extends Component {
     this.setState(Object.assign({ loading: false }, json.pets[0]));
   }
 
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+  buyAnimal = () =>
+    (window.location =
+      "https://www.petfinder.com/search/dogs-for-adoption/?distance=100");
+
   render() {
     if (this.state.loading) {
       return <h2>loading...</h2>;
     }
 
-    const { animal, breed, city, state, description, name, images } =
+    const { animal, breed, city, state, description, name, images, showModal } =
       this.state;
 
     //throw new Error("error thrown here");
@@ -30,7 +37,10 @@ class Details extends Component {
         <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
         <ThemeContext.Consumer>
           {([theme]) => (
-            <button style={{ backgroundColor: theme, color: "#fff" }}>
+            <button
+              style={{ backgroundColor: theme, color: "#fff" }}
+              onClick={this.toggleModal}
+            >
               Buy {animal}
             </button>
           )}
@@ -38,6 +48,18 @@ class Details extends Component {
         <p>{description}</p>
 
         <Carousel images={images} />
+
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to buy {name}</h1>
+              <div className={classes.buttons}>
+                <button onClick={this.buyAnimal}>Yes</button>
+                <button onClick={this.toggleModal}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     );
   }
