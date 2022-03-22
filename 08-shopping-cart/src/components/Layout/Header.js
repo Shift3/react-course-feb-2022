96595b9ad1e1cff65d0d4769407252d5a63fad3e
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-03-22 19:55:47 -0400

    feat(shopping-cart): Displaying the cart details and closing it

--- 08-shopping-cart/src/components/Layout/Header.js
+++ 08-shopping-cart/src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
     <>
       <header className={classes.header}>
         <h1>Delicious Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
 
       <div className={classes['main-image']}>
