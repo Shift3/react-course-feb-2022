f2cdd84d2da58d41a3dc3cefe3952be71771a653
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-03-22 18:44:55 -0400

    feat(shopping-cart): Display meals item

--- 08-shopping-cart/src/components/Meals/MealsSummary.js
+++ 08-shopping-cart/src/components/Meals/MealsSummary.js
@@ -7,9 +7,6 @@ const MealsSummary = () => {
       <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut tortor fermentum, molestie mauris convallis, pharetra ante. Quisque pretium, mauris ac luctus accumsan, lacus quam sollicitudin felis, sed lacinia enim justo id lacus. Nam auctor rutrum risus, non volutpat felis varius eget. Fusce imperdiet ante sed faucibus scelerisque. Sed massa risus, porttitor in velit et, convallis rhoncus sapien. Vestibulum faucibus elit diam, sed consectetur est dapibus vitae. Mauris condimentum quam ac lacus euismod ultricies. Sed vitae tellus vitae magna iaculis euismod. Vivamus lacinia posuere rutrum.
       </p>
-      <p>
-      Pellentesque sagittis tortor lorem, at efficitur nisi maximus ut. Aliquam dictum id tellus et tempor. Etiam vitae lacus aliquet, tristique elit et, tempor sem. Nulla efficitur vel sem quis elementum. Vivamus tempor elit ante, sit amet molestie leo dictum nec. Phasellus in convallis massa, a mattis augue. Donec interdum egestas facilisis. Fusce semper, neque mattis commodo malesuada, mi est sagittis justo, sit amet malesuada justo enim ac purus. Cras vitae varius justo. Curabitur massa eros, placerat id magna non, gravida dignissim nibh.
-      </p>
     </section>
   );
 }
