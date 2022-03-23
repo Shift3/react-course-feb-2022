c07883371624e7dc53c92010c7c499624088142d
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-03-22 21:16:51 -0400

    feat(shopping-cart): Use context to add items to cart

--- 08-shopping-cart/src/components/Layout/HeaderCartButton.js
+++ 08-shopping-cart/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
+import { useContext } from 'react';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  });
+
   return(
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>You Cart</span>
-      <span className={classes.badge}>5</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 }
--- 08-shopping-cart/src/components/Meals/MealItem/MealItem.js
+++ 08-shopping-cart/src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,21 @@
+import { useContext } from 'react';
 import classes from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
+  const cartCtx = useContext(CartContext);
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount,
+      price: props.price
+    });
+  };
+
   return(
     <li className={classes.meal}>
       <div>
@@ -15,7 +28,7 @@ const MealItem = (props) => {
         </div>
       </div>
       <div>
-        <MealItemForm id={props.id}/>
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler}/>
       </div>
     </li>
   );
